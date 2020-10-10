2ebcf6374f334c41be7bbad962a41c0f6a14c087
zhanghaifei <872125394@qq.com>
2020-10-10 17:16:20 +0800

    test

--- .tea/entryFiles-development/index$.web.js
+++ .tea/entryFiles-development/index$.web.js
@@ -3,3 +3,4 @@ require('./config$');
 
 require('../../pages/index/index?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
 require('../../pages/test/test?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../pages/gitTest/gitTest?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
