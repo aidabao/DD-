9fa33fee46c5c2876bacca6ed24be4a26ef6351d
zhanghaifei <872125394@qq.com>
2020-10-14 14:40:51 +0800

    456

--- .tea/entryFiles-development/index$.web.js
+++ .tea/entryFiles-development/index$.web.js
@@ -4,3 +4,4 @@ require('./config$');
 require('../../pages/index/index?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
 require('../../pages/test/test?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
 require('../../pages/gitTest/gitTest?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../pages/git/git?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
--- .tea/entryFiles-development/index$.worker.js
+++ .tea/entryFiles-development/index$.worker.js
@@ -35,6 +35,7 @@ require('../../app');
 require('../../pages/index/index?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
 require('../../pages/test/test?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
 require('../../pages/gitTest/gitTest?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../pages/git/git?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
 }
 self.bootstrapApp ? self.bootstrapApp({ success }) : success();
 }
